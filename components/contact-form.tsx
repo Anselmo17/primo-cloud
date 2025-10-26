e6797c131fc6952f58c2564d7d356ea697f094d2
Anselmo <ansinftec17@gmail.com>
2025-10-26 08:19:20 -0300

    FEAT:Ajuste no texto do formulario de contatos

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -132,7 +132,7 @@ export function ContactForm() {
           id="message"
           rows={5}
           className="w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 dark:bg-gray-800"
-          placeholder="sua Mensagem"
+          placeholder="sua mensagem"
           value={formData.message}
           onChange={handleChange}
           required
